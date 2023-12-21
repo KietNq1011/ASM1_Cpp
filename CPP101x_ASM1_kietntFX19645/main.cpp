4f556a2bd21bbcddde10ae1de8a2c1c4bd34e35a
Nguyễn Tuấn Kiệt <118150748+KietNq1011@users.noreply.github.com>
2023-12-21 15:37:14 +0700

    Add files via upload

--- CPP101x_ASM1_kietntFX19645/main.cpp
+++ CPP101x_ASM1_kietntFX19645/main.cpp
@@ -2,19 +2,68 @@
 #include <cctype>
 #include <string>
 
-
 using namespace std;
 
-// MH_1 check key
+int speed = 0;
+int previous_choice = 0;
+
+// Function declarations
+void check_key();
+int menu_MH2();
+void gear_P();
+void gear_N();
+void gear_R();
+void gear_D();
+int menu_MH4();
+void check_speed(int choice);
+
+int main() {
+    check_key(); // call fn to check password
+
+    while (true) {
+        int choice = menu_MH2();
+        if (choice == 1) {
+            gear_P();
+        }
+        else if (choice == 2) {
+            gear_R();
+        }
+        else if (choice == 3) {
+            gear_N();
+        }
+        else if (choice == 4) {
+            gear_D();
+        }
+        else if (choice == 5) {
+            system("cls");
+            cout << "XE DA DUOC TAT. TAM BIET" << endl;
+            return 0;
+        }
+        else {
+            system("cls");
+            cin.clear(); // delete error cin status
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cout << "Lua chon khong hop le. Vui long nhap lai." << endl;
+            system("pause");
+            continue;
+        }
+
+        check_speed(choice);
+
+        system("pause");  // Pause the screen for the user to read messages
+    }
+
+    return 0;
+}
+
 void check_key()
 {
-    string password; // Personal identification number
-    // Loop until the correct personal identification number is entered (default is 12345678)
+    string password;
     while (true) {
         cout << "Nhap ma so ca nhan: ";
         cin >> password;
 
-        if (password == "123") {
+        if (password == "12345678") {
             break;
         }
         else {
@@ -25,11 +74,9 @@ void check_key()
     }
 }
 
-// MH_2 
 int menu_MH2()
 {
-    system("cls");  // Clear the screen
-
+    system("cls");
     cout << "XIN MOI LUA CHON:" << endl;
     cout << "1. P" << endl;
     cout << "2. R" << endl;
@@ -41,13 +88,25 @@ int menu_MH2()
     cout << "Choice: ";
     cin >> choice;
 
+    // If switching from R to D and speed is not 0, display a warning
+    if (previous_choice == 2 && choice == 4 && speed != 0) {
+        cout << "VUI LONG CHUYEN TOC DO VE 0 TRUOC KHI CHUYEN SO" << endl;
+        choice = previous_choice;
+        system("pause");
+    }
+    else if (previous_choice == 4 && choice == 2 && speed != 0) {
+        cout << "VUI LONG CHUYEN TOC DO VE 0 TRUOC KHI CHUYEN SO" << endl;
+        choice = previous_choice;
+        system("pause");
+    }
+
+    previous_choice = choice;
     return choice;
 }
 
-// Gear P - using pointer
-void gear_P(int* speed)
+void gear_P()
 {
-    if (*speed == 0) {
+    if (speed == 0) {
         system("cls");
         cout << "Da ve so P." << endl;
         cout << "CHU Y SU DUNG PHANH TAY DE DAM BAO AN TOAN" << endl << endl;
@@ -58,10 +117,9 @@ void gear_P(int* speed)
     }
 }
 
-// Gear N
-void gear_N(int* speed)
+void gear_N()
 {
-    if (*speed == 0) {
+    if (speed == 0) {
         system("cls");
         cout << "Da ve so N (Tam dung xe)." << endl;
     }
@@ -71,120 +129,96 @@ void gear_N(int* speed)
     }
 }
 
-// Gear R
-void gear_R(int* speed)
+void gear_R()
 {
-    system("cls");
-    cout << "Da ve so R." << endl;
-    *speed = 7;
+    if (speed == 0) {
+        system("cls");
+        cout << "Da ve so R." << endl;
+        speed = 7;
+    }
+    else {
+        system("cls");
+        cout << "Da ve so R." << endl;
+        if (speed > 20)
+            cout << "CANH BAO: Toc do qua cao cho lai xe!" << endl;
+    }
 }
 
-// Gear D
-void gear_D(int* speed)
+void gear_D()
 {
-
-    system("cls");
-    cout << "Da ve so D." << endl;
-    *speed = 7;
+    if (speed == 0) {
+        system("cls");
+        cout << "Da ve so D." << endl;
+        speed = 7;
+    }
+    else {
+        system("cls");
+        cout << "Da ve so D." << endl;
+        if (speed > 60)
+            cout << "CANH BAO: Toc do qua cao cho lai xe!" << endl;
+    }
 }
 
-// Option MH_4
-int menu_MH4(int* speed)
+int menu_MH4()
 {
     int action;
-    cout << "TOC DO HIEN TAI: " << *speed << "km/h" << endl;
-    cout << "MOI LUA CHON" << endl;
-    cout << "1. TANG TOC" << endl;
-    cout << "2. GIAM TOC" << endl;
-    cout << "3. PHANH" << endl;
-    cout << "4. TRO VE MENU" << endl;
-
-    cin >> action;
+    while (true)
+    {
+        cout << "TOC DO HIEN TAI: " << speed << "km/h" << endl;
+        cout << "MOI LUA CHON" << endl;
+        cout << "1. TANG TOC" << endl;
+        cout << "2. GIAM TOC" << endl;
+        cout << "3. PHANH" << endl;
+        cout << "4. TRO VE MENU" << endl;
+        cout << "Choice: ";
+        cin >> action;
+        if (action <= 4 && action >= 1)
+        {
+            break;
+        }
+        else {
+            cin.clear(); // Xóa trạng thái lỗi của cin 
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');// Loại bỏ dữ liệu không hợp lệ từ bộ đệm đầu vào
+            cout << "Lua chon khong hop le. Vui long nhap lai." << endl;
+            system("pause");
+            system("cls");
+            continue;
+        }
+    }
 
     return action;
 }
 
-// Check speed and display warnings
-void check_speed(int* speed, int choice)
+void check_speed(int choice)
 {
-    if ((*speed > 0) && ((choice == 2) || (choice == 4))) {
+    if ((speed > 0) && ((choice == 2) || (choice == 4))) {
         while (true) {
-            int action = menu_MH4(speed);
+            int action = menu_MH4();
 
             if (action == 1) {
-                *speed += 5;
+                speed += 5;
             }
             else if (action == 2) {
-                *speed -= 5;
+                speed -= 5;
             }
             else if (action == 3) {
                 system("cls");
-                *speed = 0;
+                speed = 0;
             }
             else
-                break; // Exit the loop if the user chooses to return to the menu.
+                break;
 
-            // Check speed and display warnings
-            // If speed is below 60 and below 20 (for R)
-            if ((choice == 4 && *speed < 60 && *speed > 0) || (choice == 2 && *speed < 20 && *speed > 0)) {
-               system("cls");
+            if ((choice == 4 && speed < 60 && speed > 0) || (choice == 2 && speed < 20 && speed > 0)) {
+                system("cls");
             }
-            // If speed > 60 km/h (for D) or 20 km/h (for R), issue a high-speed warning to the driver
-            else if ((choice == 4 && *speed >= 60) || (choice == 2 && *speed >= 20))
-            {
+            else if ((choice == 4 && speed >= 60) || (choice == 2 && speed >= 20)) {
                 system("cls");
                 cout << "CANH BAO: Toc do qua cao cho lai xe!" << endl;
             }
-            // Check if the user decelerates to a speed below 0
-            else if (*speed <= 0)
-            {
+            else if (speed <= 0) {
                 system("cls");
-                *speed = 0;
+                speed = 0;
             }
         }
     }
 }
-
-int main() {
-    int speed = 0;  // Initial speed
-    int* speed_ptr = &speed;
-
-    check_key(); // call fn to check password
-
-    while (true) {
-        int choice = menu_MH2();
-        if (choice == 1) {
-            gear_P(speed_ptr);
-        }
-        else if (choice == 2) {
-            gear_R(speed_ptr);
-        }
-        else if (choice == 3) {
-            gear_N(speed_ptr);
-        }
-        else if (choice == 4) {
-            gear_D(speed_ptr);
-        }
-        else if (choice == 5) {
-            system("cls");
-            cout << "XE DA DUOC TAT. TAM BIET" << endl;
-            return 0;
-        }
-        else {
-            system("cls");
-            cin.clear(); // Xóa trạng thái lỗi của cin 
-            cin.ignore(); // Loại bỏ dữ liệu không hợp lệ từ bộ đệm đầu vào
-            cout << "Lua chon khong hop le. Vui long nhap lai." << endl;
-            system("pause");
-            continue;
-        }
-
-
-        check_speed(speed_ptr, choice);
-
-        system("pause");  // Pause the screen for the user to read messages
-    }
-
-    return 0;
-}
-
